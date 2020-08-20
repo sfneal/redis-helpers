5b8b5cdf2b1b759a955ee98d073b8b07c9d2c858
Stephen Neal <stephen.neal14@gmail.com>
2020-08-20 12:45:55 -0400

    Apply fixes from StyleCI (#2)

--- src/helpers.php
+++ src/helpers.php
@@ -217,9 +217,8 @@ function redisClearCache()
     return redisDelete('');
 }
 
-
 /**
- * Pass a $callback function to be stored in the Cache for an amount of time
+ * Pass a $callback function to be stored in the Cache for an amount of time.
  *
  * @param string $key
  * @param int $ttl
@@ -231,9 +230,8 @@ function redisRemember(string $key, int $ttl, Closure $callback)
     return Cache::remember($key, $ttl, $callback);
 }
 
-
 /**
- * Pass a $callback function to be stored in the Cache forever
+ * Pass a $callback function to be stored in the Cache forever.
  *
  * @param string $key
  * @param Closure $callback
