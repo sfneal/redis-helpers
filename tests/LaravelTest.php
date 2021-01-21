7f879d3e08d319693fdd0f3bd02ef4d541816711
Stephen Neal <stephen.neal14@gmail.com>
2021-01-21 16:24:06 -0500

    MAKE LaravelTest for testing ServiceProvider & config
    ADD orchestra/testbench to composer dev requires.

---
+++ tests/LaravelTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Sfneal\Helpers\Redis\Tests;
+
+use Illuminate\Foundation\Application;
+use Orchestra\Testbench\TestCase;
+use Sfneal\Helpers\Redis\Providers\RedisHelpersServiceProvider;
+
+class LaravelTest extends TestCase
+{
+    /**
+     * Register package service providers.
+     *
+     * @param Application $app
+     * @return array|string
+     */
+    protected function getPackageProviders($app)
+    {
+        return RedisHelpersServiceProvider::class;
+    }
+
+    /** @test */
+    public function config_is_accessible()
+    {
+        // Confirm the redis-helper config array exists
+        $this->assertIsArray(config('redis-helpers'));
+    }
+
+    /** @test */
+    public function config_prefix()
+    {
+        // Confirm the default config 'prefix' key is correct
+        $output = config('redis-helpers.prefix');
+        $expected = 'app';
+        $this->assertTrue($output == $expected);
+    }
+
+    /** @test */
+    public function config_ttl()
+    {
+        // Confirm the default config 'prefix' key is correct
+        $output = config('redis-helpers.ttl');
+        $expected = 3600;
+        $this->assertIsInt($output);
+        $this->assertTrue($output == $expected);
+    }
+}
