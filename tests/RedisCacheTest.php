c7245bd337b291f903f55dbe9bbb9d1dbc8151e5
Stephen Neal <stephen.neal14@gmail.com>
2021-01-26 14:43:02 -0500

    ADD test methods for remember & rememberForever

--- tests/RedisCacheTest.php
+++ tests/RedisCacheTest.php
@@ -183,4 +183,24 @@ class RedisCacheTest extends TestCase
             $this->assertTrue(RedisCache::missing($key));
         }
     }
+
+    public function test_remember()
+    {
+        $key = 'keytoremember';
+        RedisCache::remember($key, 100, function () {
+            return md5(random_int(1000, 2000));
+        });
+
+        $this->assertTrue(RedisCache::exists($key));
+    }
+
+    public function test_rememberForever()
+    {
+        $key = 'keytorememberforever';
+        RedisCache::rememberForever($key, function () {
+            return md5(random_int(2000, 3000));
+        });
+
+        $this->assertTrue(RedisCache::exists($key));
+    }
 }
