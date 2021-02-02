e058e3c60b54ac1a1b6f07aa435376365bdb862c
Stephen Neal <stephen.neal14@gmail.com>
2021-02-02 10:39:06 -0500

    ADD test_keys method to RedisCacheTest

--- tests/RedisCacheTest.php
+++ tests/RedisCacheTest.php
@@ -15,6 +15,28 @@ class RedisCacheTest extends TestCase
         $this->assertTrue($value == $expected);
     }
 
+    public function test_keys()
+    {
+        $array = [
+            'bos:3#pos' => 'c',
+            'bos:3#name_first' => 'Charlie',
+            'bos:3#name_last' => 'Coyle',
+            'bos:3#age' => 26,
+            'bos:12#pos' => 'w',
+            'bos:12#name_first' => 'Craig',
+            'bos:12#name_last' => 'Smith',
+            'bos:12#age' => 32,
+        ];
+        RedisCache::setMany($array);
+
+        $keys = RedisCache::keys('bos:3');
+
+        $this->assertCount(4, $keys);
+        foreach (['bos:3#pos', 'bos:3#name_first', 'bos:3#name_last', 'bos:3#age'] as $key) {
+            $this->assertTrue(in_array($key, $keys));
+        }
+    }
+
     public function test_get()
     {
         $key = 'bos-33';
