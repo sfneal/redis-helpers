352f3d327f81d754380c8e475913f0842c9f53b3
Stephen Neal <stephen.neal14@gmail.com>
2021-08-25 10:24:32 -0400

    ADD use of dynamic image tag names so that images are named with a "$PHP_VERSION-$GIT_BRANCH" tag

---
+++ boot.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+PHP_VERSION=$(php --version)
+PHP_VERSION=${PHP_VERSION:4:3}
+
+TAG="$PHP_VERSION-$BRANCH"
+export TAG
+
+echo "Building image: stephenneal/redis-helpers:${TAG}"
+docker-compose build
+
+docker compose up -d
+
+docker logs -f package
+
+sleep 60
+docker-compose down -v --remove-orphans
