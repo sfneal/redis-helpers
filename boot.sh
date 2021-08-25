d74575979a071e4242e50756ece0a75c8e9c4392
Stephen Neal <stephen.neal14@gmail.com>
2021-08-25 13:08:37 -0400

    ADD conditional to confirm the 'package' container exited with code 0

--- boot.sh
+++ boot.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# exit when any command fails
+set -e
+
 COMPOSER_FLAGS=${1:-""}
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
@@ -9,6 +12,8 @@ PHP_VERSION=${PHP_VERSION:4:3}
 
 TAG="$PHP_VERSION-$BRANCH"
 export TAG
+
+docker-compose down -v --remove-orphans
 
 echo "Building image: stephenneal/redis-helpers:${TAG}"
 docker build -t stephenneal/redis-helpers:"${TAG}" \
@@ -20,7 +25,17 @@ docker compose up -d
 
 docker logs -f package
 
-sleep 20
-docker-compose down -v --remove-orphans
+while true; do
+    if [[ $(docker inspect -f '{{.State.Running}}' package) != true ]]; then
+        break
+    else
+        echo "'package' container is still running... waiting 3 secs then checking again..."
+        sleep 3
+    fi
+done
 
+# Confirm it exited with code 0
+docker inspect -f '{{.State.ExitCode}}' package > /dev/null 2>&1
+
+# Confirm the image exists
 docker image inspect stephenneal/redis-helpers:"${TAG}" > /dev/null 2>&1
