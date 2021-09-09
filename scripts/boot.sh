6ba3d36445697bdf8d4e70911cafaa1848287cb4
Stephen Neal <stephen.neal14@gmail.com>
2021-09-09 15:39:25 -0400

    START making generic boot script that doesn't need to be modified for different repos

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -7,35 +7,44 @@ set -e
 
 COMPOSER_FLAGS=${1:-""}
 
+# Git Branch
 if [ -z "$TRAVIS_BRANCH" ]; then
     BRANCH=$(git rev-parse --abbrev-ref HEAD)
 else
     BRANCH="${TRAVIS_BRANCH}"
 fi
 
+# Repo name
+REPO="redis-helpers"
+
+# Docker username
+DOCKER_USERNAME="stephenneal"
+
+# PHP Version
 PHP_VERSION=$(php --version)
 PHP_VERSION=${PHP_VERSION:4:3}
 
 # Allow for a php-composer image tag argument
 PHP_COMPOSER_TAG=${2-$PHP_VERSION}
 
+# Export Docker image Tag
 TAG="$PHP_COMPOSER_TAG-$BRANCH"
 export TAG
 
 docker-compose down -v --remove-orphans
 
-echo "Building image: stephenneal/redis-helpers:${TAG}${COMPOSER_FLAGS:8}"
-docker build -t stephenneal/redis-helpers:"${TAG}${COMPOSER_FLAGS:8}" \
+echo "Building image: ${DOCKER_USERNAME}/${REPO}:${TAG}${COMPOSER_FLAGS:8}"
+docker build -t ${DOCKER_USERNAME}/${REPO}:"${TAG}${COMPOSER_FLAGS:8}" \
     --build-arg php_composer_tag="${PHP_COMPOSER_TAG}" \
     --build-arg composer_flags="${COMPOSER_FLAGS}" \
      .
 
 docker-compose up -d
 
-docker logs -f redis-helpers
+docker logs -f ${REPO}
 
 while true; do
-    if [[ $(docker inspect -f '{{.State.Running}}' redis-helpers) != true ]]; then
+    if [[ $(docker inspect -f '{{.State.Running}}' "${REPO}") != true ]]; then
         break
     else
         echo "'package' container is still running... waiting 3 secs then checking again..."
@@ -44,7 +53,7 @@ while true; do
 done
 
 # Confirm it exited with code 0
-docker inspect -f '{{.State.ExitCode}}' redis-helpers > /dev/null 2>&1
+docker inspect -f '{{.State.ExitCode}}' "${REPO}" > /dev/null 2>&1
 
 # Confirm the image exists
-docker image inspect stephenneal/redis-helpers:"${TAG}" > /dev/null 2>&1
+docker image inspect ${DOCKER_USERNAME}/"${REPO}":"${TAG}" > /dev/null 2>&1
